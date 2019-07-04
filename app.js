5a8b934ea668623b39db5071858e1c2a2ac36fc8
Helder Castrillón <heldersoft@gmail.com>
2019-07-04 17:12:38 -0500

    Update app.js
    change promise to await method

--- app.js
+++ app.js
@@ -3,21 +3,32 @@
 const setting=require("./setting.json")
 const DhisQuery =require('./modules/DhisQuery')
 //create object
-let DHISAppQuery = new DhisQuery(setting.primaryServer)
-DHISAppQuery.getProgramIndicators().then(pr=>{
-      pr.programIndicators.forEach(indicator => {
-        if(indicator.aggregateExportCategoryOptionCombo!=undefined){ // 
-                let de=indicator.aggregateExportCategoryOptionCombo.split(".")[0]//DataElement
-                let co=indicator.aggregateExportCategoryOptionCombo.split(".")[1]//CategoryCombo
+let DHISAppQuery = new DhisQuery(setting.testing)
+async function settingParameters(id,de,co){
+        let dv = await DHISAppQuery.getDataValueProgramIndicators(id);
+        console.log(dv.rows)
+        dv.rows.forEach(MetaValue=>{
+                let pe= MetaValue[0]
+                let ou= MetaValue[1]
+                let value=MetaValue[2]
+                console.log(pe)
+                DHISAppQuery.setDataValue(de,pe,co,ou,value)
+        }) 
 
-                DHISAppQuery.getDataValueProgramIndicators(indicator.id).then(dv=>{
-                        let pe= dv.rows[0][0]
-                        let ou= dv.rows[0][1]
-                        let value=dv.rows[0][2]
-                        DHISAppQuery.setDataValue(de,pe,co,ou,value)
-                       
-                })
-        }
-      }); 
-})
+}
+async function _run(){
+        let pr=await DHISAppQuery.getProgramIndicators();
+        pr.programIndicators.forEach(indicator => {
+                if(indicator.aggregateExportCategoryOptionCombo!=undefined){ // 
+                        let de=indicator.aggregateExportCategoryOptionCombo.split(".")[0]//DataElement
+                        let co=indicator.aggregateExportCategoryOptionCombo.split(".")[1]//CategoryCombo
+                        settingParameters(indicator.id,de,co)
+                }
+        });        
+}
+//start Script
+_run()
+
+
+
 
